6c7dc0f5d26d0398e33eb23d4d72233688ea8d11
Harrison Lingren <harrisonlingren@gmail.com>
2017-10-04 22:51:23 +0100

    form flow, search to populate form, etc

--- assignment_1/js/form.js
+++ assignment_1/js/form.js
@@ -3,19 +3,21 @@ let apiString = 'https://com601-assign1.herokuapp.com/api';
 
 $(document).ready(() => {
     // switch view to search
-    $('#search-link').click(() => {
+    $('.search-link').click(() => {
         $('.booking-form').hide();
         $('.search-form').show();
     })
 
     // switch view to booking form
-    $('#booking-link').click(() => {
+    $('.booking-link').click(() => {
         $('.booking-form').show();
         $('.search-form').hide();
+        $('.success-page').hide();
     })
 
-    // hide booking form
+    // hide booking form and success page
     $('.booking-form').hide();
+    $('.success-page').hide();
 
     // add click handler for submit button
     let submitBtn = $('#submit-btn');
@@ -56,29 +58,33 @@ $(document).ready(() => {
 // handle submit to create new booking
 // TODO: handle updating booking
 function submitForm(booking) {
-
+    let query = apiString + '/create';
     let postData = {
         'first': booking.first,
         'last': booking.last,
         'email': booking.email
     };
-
-    let query = apiString + '/create';
+    
     console.log('querying: %s', query);
-    /* $.post(query, postData, (result) => {
-        // TODO: change this to display better
-        console.log(result);
-    }); */
 
-    $.ajax({
-        url: query,
-        type: 'POST',
-        data: postData,
-        success: (res, status) => {
-            console.log(res, status);
-        },
-        error: (res) => {
-            console.log(res.status, res.statusText);
+    $.post(query, postData, (result, status, resp) => {
+        // TODO: change this to display better
+        console.log(result, status, resp.statusText, resp.status);
+        if (resp.status == 201) {
+            // process success page
+            $('#booked-id').append(result.data.id);
+            $('#booked-name').append(result.data.first + ' ' + result.data.last);
+            $('#booked-email').append(result.data.email);
+
+            $('.booking-form').hide();
+            $('.success-page').show();
+        } else {
+            // error tooltips
+            if ($('#errormsg').length == 0) {
+                $('.booking-form .section-wrapper').append('<p id="errormsg">Looks like there was an error, %s %s! Try again.</p>', resp.status, resp.statusText);
+            } else {
+                // TODO: make this all better
+            }
         }
     });
 }
@@ -87,7 +93,16 @@ function findByRef(id) {
     let query = apiString + '/booking/' + id;
     console.log('querying: %s', query);
     $.getJSON(query, (result) => {
-        // TODO: change this to display better
-        console.log(result);
+        console.log(result, result.data);
+        if (result.data) {
+            $('#firstname').val(result.data.first);
+            $('#lastname').val(result.data.last);
+            $('#email').val(result.data.email);
+
+            $('.booking-form').show();
+            $('.search-form').hide();
+        } else {
+            console.log('not found!');
+        }
     });
 }
